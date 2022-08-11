f99da489e07c53ef838ccec57e69227a9d447237
penaam <penaam@rose-hulman.edu>
2022-08-10 21:40:27 -0400

    Demontime babay

--- main.py
+++ main.py
@@ -8,11 +8,15 @@ class MikeDemonSlayer:
         self.x = x
         self.y = y
         self.bullets = []
+        self.image = None
 
     def draw(self):
+        pygame.draw
 
     def primary_fire(self):
-        new_bullet =
+        new_bullet = bullet_neutral(self.screen,self.image.get_width + 1, self.y)
+        self.bullets.append(new_bullet)
+        # add sound?
 
 class bullet_neutral:
     def __init__(self, screen, x, y, speed, size, length):
@@ -23,6 +27,7 @@ class bullet_neutral:
         self.size = int(size)
         self.len = int(length)
         self.has_boomed = False
+
 
     def move(self):
         self.x += self.speed
@@ -44,10 +49,17 @@ def main():
             pressed_keys = pygame.key.get_pressed()
             if event.type == pygame.QUIT:
                 sys.exit()
+            if event.type == pygame.KEYDOWN and pressed_keys[pygame.K_SPACE]:
+                MikeDemonSlayer.primary_fire()
+
 
 
         screen.fill((0, 0, 0))
+
         pressed_keys = pygame.key.get_pressed()
+        if pressed_keys[pygame.K_UP]:
+            MikeDemonSlayer.y -= 5
+
 
 
 main()
